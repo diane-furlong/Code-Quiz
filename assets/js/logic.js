64fc4eb55742ed28e8864d4f30b0e256cf71c1a0
Diane Furlong <dfurlongm@gmail.com>
2020-12-13 12:59:36 -0500

    starting pseudocoding

---
+++ assets/js/logic.js
@@ -0,0 +1,17 @@
+//define the variables
+var startBtn = document.getElementById("start"); //this is the start button
+var startScreen = document.getElementById("start-screen"); //this is the start screen div
+var qScreen = document.getElementById("questions"); //this is the questions div
+var endScreen = document.getElementById("end-screen"); //this is the end screen div
+var countdown = document.getElementById("time"); //this is the time element on the timer
+var interval; //this sets the interval on the timer
+var
+//create function to start the quiz
+function startQuiz(){
+    //hide startScreen
+    //show questions
+    //start countdown
+}
+
+//calling the function to start the quiz on click of the start button
+startBtn.addEventListener("click", startQuiz());
